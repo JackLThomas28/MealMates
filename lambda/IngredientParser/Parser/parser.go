51a8e7a5f186fe70fe2614f1e6875737b0028214
Jack  Thomas <jacklthomas28@gmail.com>
2022-01-28 09:16:47 -0700

    Add error path to ingredient parser

--- lambda/IngredientParser/Parser/parser.go
+++ lambda/IngredientParser/Parser/parser.go
@@ -84,10 +84,42 @@ func printIngredient(i Ingredient) {
 }
 
 func convertStringToFloat(strAmount string) float64 {
-	if strAmount == "⅓" {
-		return 0.33
+	if strAmount == "¼" {
+		return 0.25
 	} else if strAmount == "½" {
 		return 0.5
+	} else if strAmount == "¾" {
+		return 0.75
+	} else if strAmount == "⅐" {
+		return 0.142857142857143
+	} else if strAmount == "⅑" {
+		return 0.111111111111111
+	} else if strAmount == "⅒" {
+		return 0.1
+	} else if strAmount == "⅓" {
+		return 0.333333333333333
+	} else if strAmount == "⅔" {
+		return 0.666666666666667
+	} else if strAmount == "⅕" {
+		return 0.2
+	} else if strAmount == "⅖" {
+		return 0.4
+	} else if strAmount == "⅗" {
+		return 0.6
+	} else if strAmount == "⅘" {
+		return 0.8
+	} else if strAmount == "⅙" {
+		return 0.166666666666667
+	} else if strAmount == "⅚" {
+		return 0.833333333333333
+	} else if strAmount == "⅛" {
+		return 0.125
+	} else if strAmount == "⅜" {
+		return 0.375
+	} else if strAmount == "⅝" {
+		return 0.625
+	} else if strAmount == "⅞" {
+		return 0.875
 	} else {
 		amt, err := strconv.ParseFloat(strAmount, 64)
 		if err != nil {
@@ -98,7 +130,7 @@ func convertStringToFloat(strAmount string) float64 {
 	}
 }
 
-func calculateAmount(rawIngredient string) float64 {
+func calculateAmount(rawIngredient string) (float64, error) {
 	amount := strings.Split(rawIngredient, " ")[0]
 
 	// Check for decimals, numbers, fractions
@@ -111,21 +143,30 @@ func calculateAmount(rawIngredient string) float64 {
 	for i := 0; i < len(resultAmt); i++ {
 		// Get the slice containing the string num
 		strAmount := amount[resultAmt[i][0]:resultAmt[i][1]]
+		// Convert from string to float
 		amt = amt + convertStringToFloat(strAmount)
 	}
 
+	// Find any parentheses
 	rePar, _ := regexp.Compile(RE_PAR_PATTERN)
 	resultPar := rePar.FindAllIndex([]byte(rawIngredient), 2)
 
 	if len(resultAmt) != 0 && len(resultPar) != 0 {
 		lastIndexOfAmt := resultAmt[0][1]
 		firstIndexOfParen := resultPar[0][0]
+		// Check to make sure that the parentheses are immediately after amount
+		// Good Ex: 1 (3 pound) chickent
+		// Bad  Ex: 3 cups cheese (shredded)
 		if lastIndexOfAmt == firstIndexOfParen - 1 {
+			// Get the text from inside the parentheses
 			splitParText := strings.Split(rawIngredient[resultPar[0][1]:resultPar[1][0]], " ")
+			// The amount should be the first thing from parentheses
+			// Ex: (3 pound)
+			//      ^
 			parAmt, err := strconv.ParseFloat(splitParText[0], 64)
 			if err != nil {
-				fmt.Println("Error:", err)
-				parAmt = 1
+				return parAmt, err
+				// parAmt = 1
 			}
 			amt = amt * parAmt
 		}
@@ -133,21 +174,25 @@ func calculateAmount(rawIngredient string) float64 {
 	if amt == 0 {
 		amt = 1
 	}
-	return amt
+	return amt, nil
 }
 
-func getUnit(rawIngredient string) string {
+func getUnit(rawIngredient string) (string, error) {
 	for word, unit := range getUnitsMap() {
+		// Find the unit. May be plural
+		// Ex: pounds
 		re, _ := regexp.Compile(`(\b` + word + `)(s\b|\b)`)
 		result := re.FindAllIndex([]byte(rawIngredient), -1)
+	
 		if len(result) != 0 {
-			return unit
+			return unit, nil
 		}
 	}
-	return "unit"
+	// If we didn't find it, return generic
+	return "unit", nil
 }
 
-func determineName(rawIngredient string) string {
+func determineName(rawIngredient string) (string, error) {
 	re, _ := regexp.Compile(",")
 	commas := re.FindAllIndex([]byte(rawIngredient), -1)
 
@@ -180,7 +225,7 @@ func determineName(rawIngredient string) string {
 		amount := strings.Split(rawIngredient, " ")[0]
 		re, _ = regexp.Compile(RE_AMT_PATTERN)
 		amts := re.FindAllIndex([]byte(amount), -1)
-	
+
 		// Only remove the amount if it was there
 		if len(amts) > 0 {
 			// Find the first space and remove everything before it
@@ -204,11 +249,10 @@ func determineName(rawIngredient string) string {
 			}
 		}
 	}
-	
-	return name
+	return name, nil
 }
 
-func ParseIngredients(ingredients []string) []Ingredient {
+func ParseIngredients(ingredients []string) ([]Ingredient, error) {
 	// 1. Store ingredients in Ingredient objects
 	var ingredientList []Ingredient
 	for i := 0; i < len(ingredients); i++ {
@@ -217,18 +261,29 @@ func ParseIngredients(ingredients []string) []Ingredient {
 		}
 		ingredientList = append(ingredientList, curIngr)
 	}
+
+	var err error
 	for i := 0; i < len(ingredientList); i++ {
 		// 2. Set the amount
-		ingredientList[i].Amount = calculateAmount(ingredientList[i].Raw)
+		ingredientList[i].Amount, err = calculateAmount(ingredientList[i].Raw)
+		if err != nil {
+			return nil, err
+		}
 
 		// 3. Set the unit
-		ingredientList[i].Unit = getUnit(ingredientList[i].Raw)
+		ingredientList[i].Unit, err = getUnit(ingredientList[i].Raw)
+		if err != nil {
+			return nil, err
+		}
 
 		// 4. Set the name
-		ingredientList[i].Name = determineName(ingredientList[i].Raw)
+		ingredientList[i].Name, err = determineName(ingredientList[i].Raw)
+		if err != nil {
+			return nil, err
+		}
 
 		// printIngredient(ingredientList[i])
 	}
 
-	return ingredientList
+	return ingredientList, err
 }
