5ba32569f43ecec968dfe81b464dc8a531b5db81
Jack  Thomas <jacklthomas28@gmail.com>
2022-01-28 07:56:46 -0700

    ConvertUnit implementation

---
+++ lambda/ConvertUnit/Converter/converter.go
@@ -0,0 +1,76 @@
+package converter
+
+import (
+	"errors"
+	// "fmt"
+	// "mealmates.com/lamda/ConvertUnit/Converter"
+)
+
+type Source struct {
+	val float64
+	unit Unit
+}
+
+type Destination struct {
+	val float64
+	unit Unit
+	source Source
+	err error
+}
+
+func Convert(val float64) Source {
+	// Pass along the value
+	src := Source{val: val}
+	return src
+}
+
+func (s Source) From(unitId string) (Destination) {
+	var dest Destination
+
+	if isVolumeUnit(unitId) {
+		s.unit = VolumeUnit{unit: unitId}
+	} else if isMassUnit(unitId) {
+		s.unit = MassUnit{unit: unitId}
+	} else {
+		dest = Destination{err: errors.New("Error: invalid 'From' unit provided: " + unitId)}
+	}
+
+	if dest.err == nil {
+		s.unit.setUnit(unitId)
+	
+		dest = Destination{val: s.val, source: s}
+	}	
+	return dest
+}
+
+func (d Destination) To(unitId string) (float64, error) {
+	var result float64 = 0
+
+	if d.err == nil {
+		if isVolumeUnit(unitId) {
+			d.unit = VolumeUnit{unit: unitId}
+		} else if isMassUnit(unitId) {
+			d.unit = MassUnit{unit: unitId}
+		} else {
+			d.err = errors.New("Error: invalid 'To' unit provided: " + unitId)
+		}
+
+		if d.err == nil {
+			d.unit.setUnit(unitId)
+
+			destUnitType := d.unit.getUnitType()
+			srcUnitType := d.source.unit.getUnitType()
+			if srcUnitType != destUnitType {
+				result = 0
+				d.err = errors.New("Error: mismatching unit types. Cannot convert from " + srcUnitType + " to " + destUnitType)
+			}
+		}
+
+		if d.err == nil {
+			temp := d.val * GetConvRate()[d.source.unit.getUnit()]
+			result = temp / GetConvRate()[d.unit.getUnit()]
+		}
+	}
+
+	return result, d.err
+}
---
+++ lambda/ConvertUnit/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+        "context"
+        "github.com/aws/aws-lambda-go/lambda"
+
+        // Local packages
+		"mealmates.com/lambda/ConvertUnit/Converter"
+)
+
+type MyEvent struct {
+	Amount float64 `json:"amount"`
+	To string `json:"to"`
+	From string `json:"from"`
+}
+
+type MyResponse struct {
+	Result float64 `json:"result"`
+}
+
+func HandleRequest(ctx context.Context, request MyEvent) (MyResponse, error) {
+	result, err := converter.Convert(request.Amount).From(request.From).To(request.To)
+	return MyResponse{Result: result}, err
+}
+
+func main() {
+    lambda.Start(HandleRequest)
+
+	// result, err := converter.Convert(2).From("teaspoons").To("tablespoon")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// result, err = converter.Convert(2).From("teaspoon").To("tablespoons")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// result, err = converter.Convert(2).From("pound").To("tablespoon")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// result, err = converter.Convert(20).From("pound").To("kilogram")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(result, "kilograms")	
+}
+
+// BUILD COMMAND:
+// GOOS=linux go build main.go
