5035f7f4ff33eb63a14c54f5a2dada80f7abe9c8
Jack  Thomas <jacklthomas28@gmail.com>
2022-03-20 09:17:00 -0600

    Update DBIngredient

--- lambda/objects/ingredient/ingredient.go
+++ lambda/objects/ingredient/ingredient.go
@@ -1,15 +1,14 @@
 package ingredient
 
-type Ingredient struct {
-	Name   string  `json:"name"`
-	Amount float64 `json:"amount"`
-	Unit   string  `json:"unit"`
-	Raw    string  `json:"raw"`
-}
-
 type IngredientDB struct {
 	Name string `json:"name"`
 	RecipeIds []int `json:"recipeIds"`
 	// TODO: Remove RecipeId
 	RecipeId int `json:"recipeId"`
+}
+type Ingredient struct {
+	Name   string  `json:"name"`
+	Amount float64 `json:"amount"`
+	Unit   string  `json:"unit"`
+	Raw    string  `json:"raw"`
 }
